9dbc03c6f6f7ae2ec989d17fe30365719df1f4a2
mathieu <mathieucazeres@gmail.com>
2023-12-01 19:47:45 +0100

    j'ai pas test mdr yeah

--- ApiCommerce2/src/main/java/com/ico/ApiCommerce2/repository/ProduitQuantiteRepository.java
+++ ApiCommerce2/src/main/java/com/ico/ApiCommerce2/repository/ProduitQuantiteRepository.java
@@ -6,14 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProduitQuantiteRepository extends JpaRepository<ProduitQuantite, Long> {
 
     @Modifying
+    @Transactional
     @Query("UPDATE ProduitQuantite pq SET pq.statusProduitQuantite = :status WHERE pq.id = :id")
     void updateStatusProduitQuantite(Long id, StatusProduitQuantite status);
 
     @Modifying
+    @Transactional
     @Query("UPDATE Produit p SET p.quantite =p.quantite - :quantite WHERE p = (SELECT pq.produit FROM ProduitQuantite pq WHERE pq.id = :id)")
     void updateProduitQuantite(@Param("id") Long id, @Param("quantite") int quantite);
 
